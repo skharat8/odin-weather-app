dcd1e108fdcf7d3ce4659b0cdc5ae765a6287d4e
Smitesh Kharat <5607079+skharat8@users.noreply.github.com>
2023-12-18 16:36:08 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1 align="center">Title</h1>
+<p align="center">:link: <a href="https://skharat8.github.io/title"><strong>Live Preview</strong></a></p>
+
+## Built With
+
+- HTML5
+- CSS3
+- Javascript
+
+## Acknowledgements
+
+- Inspired by [The Odin Project](https://www.theodinproject.com/lessons/)
---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
